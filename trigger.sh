f0057ab402d2ae72760690ea26ba8b06771e8adf
Harshman Kaur <harshmankaur4797.se24@chitkara.edu.in>
2026-04-20 15:35:39 +0530

    Remove unnecessary automation files

--- trigger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -c cookies.txt -s -u admin:1b15525f3e4345c098edae93d400d15a 'http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,"%22:%22",//crumb)' > crumb.txt
-CRUMB=$(cat crumb.txt | sed 's/"//g')
-curl -X POST -u admin:1b15525f3e4345c098edae93d400d15a -b cookies.txt -H "$CRUMB" 'http://localhost:8080/job/deakin-coffee-pipeline/build'
-echo "Build triggered!"
